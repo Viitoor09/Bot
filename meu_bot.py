e42c5f6a837cd933fbdeaf8fbd8de3e24c7dc451
viitoor09 <vitor.costa.s.1998@gmail.com>
2026-03-13 12:50:33 -0300

    Atualizando para o banco de dados SQLite, botões e filtros ajustados

--- meu_bot.py
+++ meu_bot.py
@@ -3,6 +3,19 @@ import requests
 import os
 from telebot import types
 from dotenv import load_dotenv
+import sqlite3
+
+conexao = sqlite3.connect('bot_dados.db', check_same_thread=False)
+cursor = conexao.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS usuarios (
+               id_telegram INTEGER PRIMARY KEY,
+               nome TEXT,
+               data_acesso TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+               )
+               ''')
+conexao.commit()
 
 
 load_dotenv()
@@ -28,15 +41,10 @@ def exibir_total(msg, d, e):
 
 @bot.message_handler(commands=["admin"])
 def ver_estatisticas(mensagem):
-    try:
-        #Abrimos o arquivo no modo "r" (read - leitura)
-        with open("usuarios.txt", "r", encoding="utf-8") as arquivo:
-            linhas = arquivo.readlines()
-            total = len(linhas)
+    cursor.execute("SELECT COUNT(*) FROM usuarios")
+    total = cursor.fetchone()[0]
+    bot.reply_to(mensagem, f"Vitor, temos um total de {total} usuario(s) registrados! ")
 
-        bot.reply_to(mensagem, f"Vitor, temos um total de {total} usuario(s) registrados! ")
-    except FileNotFoundError:
-        bot.reply_to(mensagem, "Ainda não temos nenhum usuário registrado.")
 
 #1 Decorator que responde apo comando /start ou /ajuda
 @bot.message_handler(commands=["start","ajuda"])
@@ -44,12 +52,13 @@ def responder_inicio(mensagem):
     # 1. Primeiro pega o nome do cliente
     nome_usuario = mensagem.from_user.first_name
     id_usuario = mensagem.from_user.id
+    cursor.execute("INSERT OR IGNORE INTO usuarios (id_telegram, nome) VALUES (?, ?)",
+                    (id_usuario, nome_usuario))
+    conexao.commit()
     markup = types.ReplyKeyboardMarkup(row_width=2)
     itembtn1 = types.KeyboardButton('Dólar')
     itembtn2 = types.KeyboardButton('Euro')
     markup.add(itembtn1, itembtn2)
-    with open("usuarios.txt", "a", encoding="utf-8") as arquivo:
-        arquivo.write(f"Nome: {nome_usuario} | ID: {id_usuario}\n")
     bot.send_message(mensagem.chat.id, "Escolha uma moeda para ver a cotação:", reply_markup=markup)
 
 # 2. Decorator que filtra mensagens específicos
@@ -60,7 +69,6 @@ def responder_vaga(mensagem):
 @bot.message_handler(func=lambda message:True)
 def responder_geral(mensagem):
     texto_usuario = mensagem.text.lower()
-    dinehiro = ["dolar","dólar"]
 
     if ("dólar" in texto_usuario or "dolar" in texto_usuario):
         link = "https://economia.awesomeapi.com.br/last/USD-BRL"
@@ -77,13 +85,8 @@ def responder_geral(mensagem):
         dados = requisicao.json()
         valor_euro = float(dados["EURBRL"]["bid"])
         bot.reply_to(mensagem, f"O Euro está R$ {valor_euro:.2f}")
-
-# 3. Resposta padrão para qualquer outra mensagem de texto
-@bot.message_handler(func=lambda message: True)
-def responder_padrao(mensagem):
-    bot.reply_to(mensagem, "Recebi sua mensagem! Ainda estou aprendendo a processar texto livre. Use /start para ver os comandos.")
-
-
+    else:
+        bot.reply_to(mensagem,"Não entendi")
 
 # Mantém o bot rodando e verificcando se chegaram novas mensagens
 print("Bot online... Pressione Ctrl+C para parar.")
